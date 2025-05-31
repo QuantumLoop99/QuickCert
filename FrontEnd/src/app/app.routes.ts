d4c9cf027a5d7892a48298a290cb3dc96e4c883e
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2025-05-31 23:14:22 +0530

    Create application form

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ApplicationFormComponent } from './application-form/application-form.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'application', component: ApplicationFormComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } 
 ];
