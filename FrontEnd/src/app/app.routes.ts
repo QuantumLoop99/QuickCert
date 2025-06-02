ad080504e46ab044b012a2df1b88d885717fcabe
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2025-06-02 19:12:16 +0530

    Create Feedack Page

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ApplicationFormComponent } from './application-form/application-form.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'application', component: ApplicationFormComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' } 
+  { path: 'feedback', component: FeedbackComponent },
+  { path: '', redirectTo: '/feedback', pathMatch: 'full' } 
 ];
