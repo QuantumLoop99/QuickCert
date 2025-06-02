ddd67d2c5f66de92f54a63d66a0ff753e88d4b71
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-06-02 23:25:18 +0530

    feat(auth): implement login functionality with form validation and routing

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent} from './auth/login/login.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
-    { path: '', redirectTo: 'register', pathMatch: 'full' }
+    { path: 'login', component: LoginComponent},
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
