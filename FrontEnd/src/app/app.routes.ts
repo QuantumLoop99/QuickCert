c9dd12baf42e3098c84e445123ea4ee63ef6da78
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2025-06-04 18:36:04 +0530

    Create error page

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ApplicationFormComponent } from './application-form/application-form.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'application', component: ApplicationFormComponent },
   { path: 'feedback', component: FeedbackComponent },
-  { path: '', redirectTo: '/feedback', pathMatch: 'full' } 
+  { path: 'error', component: ErrorComponent},
+  { path: '', redirectTo: '/error', pathMatch: 'full' } 
 ];
